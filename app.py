f5c0c4051482727e92a1cb72b5ed13026dcb48b2
simwawatim <simwawatimothy64@gmail.com>
2025-08-21 10:06:30 +0200

    Fix CI/CD to update Flask app correctly

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, CI/CD on Premise! Tests"
+    return "Hello, CI/CD on Premise! Updated"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
